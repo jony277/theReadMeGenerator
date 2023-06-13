f02d790957379ca83d143caa75442a0de001fba6
[Jonthan Gutierrez] <jonigutierrez741@gmail.com>
2023-06-12 23:19:01 -0400

    changed the name of the function generateREADME to generateMarkdown, changed the init fnction and refactored the code

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 
 // TODO: Create an array of questions for user input
@@ -54,40 +55,50 @@ const questions = [
   ];
 
 
+  function generateMarkdown(answers) {
+    // Generate the content for each section
+    const titleSection = `# ${answers.projectTitle}\n\n`;
+    const descriptionSection = `## Description\n\n${answers.description}\n\n`;
+    const tableOfContentsSection = `## Table of Contents\n\n- [Installation](#installation)\n- [Usage](#usage)\n- [License](#license)\n- [Contributing](#contributing)\n- [Tests](#tests)\n- [Questions](#questions)\n\n`;
+    const installationSection = `## Installation\n\n${answers.installation}\n\n`;
+    const usageSection = `## Usage\n\n${answers.usage}\n\n`;
+    const licenseSection = `## License\n\nThis application is covered under the ${answers.license} license.\n\n`;
+    const contributingSection = `## Contributing\n\n${answers.contributing}\n\n`;
+    const testsSection = `## Tests\n\n${answers.tests}\n\n`;
+    const questionsSection = `## Questions\n\nIf you have any questions, feel free to reach out to me via GitHub or email.\n\nGitHub: [${answers.githubUsername}](https://github.com/${answers.githubUsername})\nEmail: ${answers.email}\n\n`;
+  
+    // Combine all sections into the readme
+    const readmeContent =
+      titleSection +
+      descriptionSection +
+      tableOfContentsSection +
+      installationSection +
+      usageSection +
+      licenseSection +
+      contributingSection +
+      testsSection +
+      questionsSection;
+  
+    return readmeContent;
+  }
+
+
 // TODO: Create a function to write README file
-function generateREADME(answers) {
-  // Generate the content for each section
-  const titleSection = `# ${answers.projectTitle}\n\n`;
-  const descriptionSection = `## Description\n\n${answers.description}\n\n`;
-  const tableOfContentsSection = `## Table of Contents\n\n- [Installation](#installation)\n- [Usage](#usage)\n- [License](#license)\n- [Contributing](#contributing)\n- [Tests](#tests)\n- [Questions](#questions)\n\n`;
-  const installationSection = `## Installation\n\n${answers.installation}\n\n`;
-  const usageSection = `## Usage\n\n${answers.usage}\n\n`;
-  const licenseSection = `## License\n\nThis application is covered under the ${answers.license} license.\n\n`;
-  const contributingSection = `## Contributing\n\n${answers.contributing}\n\n`;
-  const testsSection = `## Tests\n\n${answers.tests}\n\n`;
-  const questionsSection = `## Questions\n\nIf you have any questions, feel free to reach out to me via GitHub or email.\n\nGitHub: [${answers.githubUsername}](https://github.com/${answers.githubUsername})\nEmail: ${answers.email}\n\n`;
-
-  // Combine all sections into the readme
-  const readmeContent =
-    titleSection +
-    descriptionSection +
-    tableOfContentsSection +
-    installationSection +
-    usageSection +
-    licenseSection +
-    contributingSection +
-    testsSection +
-    questionsSection;
-
-  return readmeContent;
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) throw err;
+    console.log('README.md file has been successfully generated!');
+  });
 }
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions).then(function(answers) {
-        console.log(answers);
-        writeToFile('README.md', answers);
-})};
+  inquirer.prompt(questions).then(function(answers) {
+    console.log(answers);
+    const readmeContent = generateMarkdown(answers);
+    writeToFile('README.md', readmeContent);
+  });
+}
 
 
 // Function call to initialize app
