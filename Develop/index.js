ccfa3ee6767817a3da8a8ea65d333ca6af074f7b
[Jonthan Gutierrez] <jonigutierrez741@gmail.com>
2023-06-12 23:49:07 -0400

    fixed a big proble after leaving behind the code that was supposed to be a test inside the index.js and removed it after a mistake was shown

--- Develop/index.js
+++ Develop/index.js
@@ -55,34 +55,6 @@ const questions = [
   ];
 
 
-  function generateMarkdown(answers) {
-    // Generate the content for each section
-    const titleSection = `# ${answers.projectTitle}\n\n`;
-    const descriptionSection = `## Description\n\n${answers.description}\n\n`;
-    const tableOfContentsSection = `## Table of Contents\n\n- [Installation](#installation)\n- [Usage](#usage)\n- [License](#license)\n- [Contributing](#contributing)\n- [Tests](#tests)\n- [Questions](#questions)\n\n`;
-    const installationSection = `## Installation\n\n${answers.installation}\n\n`;
-    const usageSection = `## Usage\n\n${answers.usage}\n\n`;
-    const licenseSection = `## License\n\nThis application is covered under the ${answers.license} license.\n\n`;
-    const contributingSection = `## Contributing\n\n${answers.contributing}\n\n`;
-    const testsSection = `## Tests\n\n${answers.tests}\n\n`;
-    const questionsSection = `## Questions\n\nIf you have any questions, feel free to reach out to me via GitHub or email.\n\nGitHub: [${answers.githubUsername}](https://github.com/${answers.githubUsername})\nEmail: ${answers.email}\n\n`;
-  
-    // Combine all sections into the readme
-    const readmeContent =
-      titleSection +
-      descriptionSection +
-      tableOfContentsSection +
-      installationSection +
-      usageSection +
-      licenseSection +
-      contributingSection +
-      testsSection +
-      questionsSection;
-  
-    return readmeContent;
-  }
-
-
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
   fs.writeFile(fileName, data, (err) => {
