0184cdf6aed7c910fc3069c0b47aaaedfdd5b1b4
[Jonthan Gutierrez] <jonigutierrez741@gmail.com>
2023-06-12 22:58:53 -0400

    added the develop file

---
+++ Develop/index.js
@@ -0,0 +1,75 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+      type: 'input',
+      name: 'projectTitle',
+      message: 'Enter the title of your project:',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Enter a description for your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Enter installation instructions for your project:',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Enter usage information for your project:',
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Enter contribution guidelines for your project:',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Enter test instructions for your project:',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license for your project:',
+      choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'None'],
+    },
+    {
+      type: 'input',
+      name: 'githubUsername',
+      message: 'Enter your GitHub username:',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address:',
+    },
+  ];
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+    {
+        if (err) throw err;
+        console.log('Success!');
+        })};
+
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions).then(function(answers) {
+        console.log(answers);
+        writeToFile('README.md', answers);
+})};
+
+
+// Function call to initialize app
+init();
